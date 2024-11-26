1d4c82f650047331dd8b06f0812e79841b456206
John Forsberg <john.h.forsberg@gmail.com>
2024-11-26 10:08:35 +0100

    Changes logger to use INFO. Adds forwarded proto to be logged.

--- mandalores/auth/backends.py
+++ mandalores/auth/backends.py
@@ -22,7 +22,8 @@ class DiscordOAuth2AuthorizationBackend(ModelBackend):
         if 'error' in request.GET or 'code' not in request.GET:
             return None
 
-        logger.warning(f'X-Forwarded-Host: {request.GET["X-Forwarded-Host"]}')
+        logger.info(f'X-Forwarded-Host: {request.GET["X-Forwarded-Host"]}')
+        logger.info(f'X-Forwarded-Proto: {request.GET["X-Forwarded-Proto"]}')
         code = request.GET['code']
         data = {
             'grant_type': 'authorization_code',
