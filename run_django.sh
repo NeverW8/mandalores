401ec363791a0f7aed4e2de295386d065f682711
John Forsberg <john.h.forsberg@gmail.com>
2024-11-26 19:58:24 +0100

    Adds log file to stdout as well for gunicorn.

--- run_django.sh
+++ run_django.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./manage.py migrate
-gunicorn --config=/mandalores/gunicorn.py -b 0.0.0.0:5000 mandalores.wsgi --workers=3 --access-logfile=- --error-logfile=- 2>&1
+gunicorn --config=/mandalores/gunicorn.py -b 0.0.0.0:5000 mandalores.wsgi --workers=3 --log-file=- --access-logfile=- --error-logfile=- 2>&1
