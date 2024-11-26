053f38cfb0e3bb53b17f655b39edb74bb4ee2593
John Forsberg <john.h.forsberg@gmail.com>
2024-11-26 21:57:33 +0100

    Adds capture output to gunicorn.

--- run_django.sh
+++ run_django.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./manage.py migrate
-gunicorn --config=/mandalores/gunicorn.py -b 0.0.0.0:5000 mandalores.wsgi --workers=3 --log-file=- --access-logfile=- --error-logfile=- 2>&1
+gunicorn --config=/mandalores/gunicorn.py -b 0.0.0.0:5000 mandalores.wsgi --workers=3 --capture-output --log-file=- --access-logfile=- --error-logfile=- 2>&1
